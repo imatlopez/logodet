8d2b028517dc5859def2517ca14b31dae83f6293
Matias Lopez <imatlopez@gmail.com>
2017-02-26 15:09:23 -0500

    clean.m

---
+++ MATLAB/clean.m
@@ -0,0 +1,16 @@
+I = double(rgb2gray(imread('IMG/zoomwalt.png')))/255;
+G = imgradient(I);
+J = I;
+for i = 1:size(I,1)
+    for j = 1:size(I,2)
+        k1 = max(1,i-1):min(size(I,1), i+1);
+        k2 = max(1,j-1):min(size(I,2), j+1);
+        g = G(k1,k2);
+        v = I(k1,k2);
+        F = v./(1+g);
+        J(i,j) = mean(F(:));
+    end
+end
+J = uint8(round(J*255));
+imshow(J)
+imsave
